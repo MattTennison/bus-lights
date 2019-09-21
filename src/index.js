e91f555568799f6b7c72d81d008379f760d4d7a9
Matt Tennison <matt@matttennison.com>
2019-09-21 16:00:20 +0100

    Use side-effect dotenv import to ensure envs are loaded first

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import cron from 'node-cron';
 
 import app from './app';
 import { app as appConfig } from './config';
 import { log } from './logger';
- 
-dotenv.config();
 
 log(`🚀 Starting polling on cron-schedule: ${appConfig.cronSchedule}`)
 cron.schedule(appConfig.cronSchedule, app.run);
